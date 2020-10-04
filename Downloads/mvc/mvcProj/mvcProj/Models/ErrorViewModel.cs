7d95f528267a9bceca6f89e079f799115a54f47f
Georgi2003 <georgidim30@gmail.com>
2020-10-04 21:21:39 +0300

    first commit

---
+++ Downloads/mvc/mvcProj/mvcProj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
